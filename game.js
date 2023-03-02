8488649adffc2cc043139fde489325ae0578e5f9
Jovan Drobnjak <drobnjakj02@gmail.com>
2023-03-02 22:19:21 +0100

    Finish the game functionality and made 5 round game
    Game function starts a 5 round game and displays the winner

--- game.js
+++ game.js
@@ -15,18 +15,47 @@ function getComputerChoice(){
 
 }
 
-playRound(playerChoice, computerChoice){
+function playRound(playerChoice, computerChoice){
 
     playerChoice= playerChoice.toLowerCase()
     computerChoice = computerChoice.toLowerCase()
-    
+
     if(playerChoice === computerChoice){
         return 'Draw'
     }
     else if((playerChoice == 'rock' && computerChoice == 'paper') || (playerChoice == 'paper' && computerChoice == 'scissors') || (playerChoice == 'scissors' && computerChoice == 'rock')){
-        return `You lost!${computerChoice.charAt(0).toUpperCase()} beats ${playerChoice} `
+        console.log(`You lost!${computerChoice} beats ${playerChoice} `)
+        return 'Lose'
     }
     else{
-        return `You win!${playerChoice.charAt(0).toUpperCase()} beats ${computerChoice}`
+        console.log(`You win!${playerChoice} beats ${computerChoice}`)
+        return 'Win'
     }
 }
+
+function game(){
+    let playerWins=0
+    let computerWins=0
+    let result;
+
+    for(let i =0;i<5;i++){
+        let playerChoice = prompt('Rock, paper or scissors?')
+        result = playRound(playerChoice,getComputerChoice())
+        if(result == "Win"){
+            playerWins++;
+        }
+        else if(result == 'Lose'){
+            computerWins++;
+        }
+    }
+
+    if(playerWins > computerWins){
+        console.log(`Player won ${playerWins} rounds, computer won ${computerWins}. Player wins!`)
+    }else if( computerWins > playerWins){
+    console.log(`Player won ${playerWins} rounds, computer won ${computerWins}. Computer wins!`)
+    }else{
+        console.log('Its a draw')
+    }
+}
+
+game()
